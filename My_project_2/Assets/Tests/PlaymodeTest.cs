90d424c5d741f14890aa999ffcde5393ed2cb27a
Seezer812 <makashov.glebas@yandex.ru>
2025-05-09 20:15:27 +0300

    Ver. 3.5

---
+++ My_project_2/Assets/Tests/PlaymodeTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlaymodeTests
+{
+    [Test]
+    public void TestExample()
+    {
+        // Простой тест, который всегда проходит
+        Assert.IsTrue(true);
+    }
+}
